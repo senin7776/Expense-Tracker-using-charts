10b26852d72b994ec3ac818305e296b5de778a92
Pratheeksha.R <pratheemaurya@gmail.com>
2026-02-13 11:37:40 +0530

    Delete frontend directory

--- frontend/script.js
+++
@@ -1,6 +0,0 @@
-const button = document.getElementById("btn");
-const message = document.getElementById("message");
-
-button.addEventListener("click", function() {
-    message.textContent = "🎉 Button Clicked Successfully!";
-});
